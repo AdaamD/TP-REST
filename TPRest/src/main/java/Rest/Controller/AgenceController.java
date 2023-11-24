7157e4b138ceee69340fffbbfdc5106c7404814d
Adam DAIA <98329603+AdaamD@users.noreply.github.com>
2023-11-24 20:28:11 +0100

    2 SW + HTML + CSS Propre et Fonctionnels

--- TPRest/src/main/java/Rest/Controller/AgenceController.java
+++ TPRest/src/main/java/Rest/Controller/AgenceController.java
@@ -1,7 +1,6 @@
 package Rest.Controller;
 
 import Rest.Data.DataInitialization;
-import Rest.Models.CarteCredit;
 import Rest.Models.Client;
 import Rest.Models.Offre;
 import Rest.Models.Reservation;
@@ -9,7 +8,8 @@ import Rest.Repository.AgenceRepository;
 import Rest.Repository.ClientRepository;
 import Rest.Repository.OffreRepository;
 import Rest.Repository.ReservationRepository;
-import Rest.Service.AvailabilityService;
+import Rest.Service.RechercheChambreService;
+import Rest.Service.ReservationService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +17,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,22 +25,24 @@ http://localhost:8080/agences/consult-availability?identifiant=yourUsername&motD
 */
 @CrossOrigin(origins = "http://localhost:8080")
 @RestController
-@RequestMapping("/agences")
+    @RequestMapping("/agences")
 public class AgenceController {
 
-    private final AvailabilityService availabilityService;
+    private final RechercheChambreService rechercheChambreService;
     private final DataInitialization dataInitialization;
     private final OffreRepository offreRepository;
     private  final ClientRepository clientRepository ;
     private final ReservationRepository reservationRepository ;
+    private final ReservationService reservationService ;
 
     @Autowired
-    public AgenceController(AvailabilityService availabilityService, DataInitialization dataInitialization, OffreRepository offreRepository, AgenceRepository agenceRepository, ClientRepository clientRepository, ReservationRepository reservationRepository) {
-        this.availabilityService = availabilityService;
+    public AgenceController(RechercheChambreService rechercheChambreService, DataInitialization dataInitialization, OffreRepository offreRepository, AgenceRepository agenceRepository, ClientRepository clientRepository, ReservationRepository reservationRepository, ReservationService reservationService) {
+        this.rechercheChambreService = rechercheChambreService;
         this.dataInitialization = dataInitialization;
         this.offreRepository = offreRepository;
         this.clientRepository = clientRepository;
         this.reservationRepository = reservationRepository;
+        this.reservationService = reservationService;
     }
 
     @GetMapping("/consulter-disponibilite")
@@ -59,7 +60,7 @@ public class AgenceController {
         }
 
         // Votre logique métier pour consulter les disponibilités
-        List<Offre> availableOffers = availabilityService.getAvailableOffersByAgence(identifiant, dateDebut, nombreLit);
+        List<Offre> availableOffers = rechercheChambreService.getAvailableOffersByAgence(identifiant, dateDebut, nombreLit);
 
 // Convertir les offres en format JSON pour la page html
         ObjectMapper objectMapper = new ObjectMapper();
@@ -80,44 +81,7 @@ public class AgenceController {
             @RequestParam String nomClient,
             @RequestParam String prenomClient) {
 
-        boolean authentificationValide = dataInitialization.validateCredentials(nomAgence, mdpAgence);
-        if (!authentificationValide) {
-            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-        }
-
-        // Vérifier l'existence de l'offre avec l'identifiant offreId
-        Optional<Offre> offreOptional = offreRepository.findById(offreId);
-        if (offreOptional.isEmpty()) {
-            return ResponseEntity.status(404).body("L'offre spécifiée n'existe pas");
-        }
-
-        // Récupérer l'offre depuis l'Optional
-        Offre offre = offreOptional.get();
-
-        // Rechercher le client dans la base de données
-        Optional<Client> existingClientOptional = clientRepository.findByNomAndPrenom(nomClient, prenomClient);
-
-        if (existingClientOptional.isEmpty()) {
-            // Le client n'existe pas dans la base, ne pas autoriser la réservation
-            return ResponseEntity.status(400).body("Le client n'est pas enregistré. Impossible de faire la réservation.");
-        }
-
-        // Obtenez le client depuis l'Optional
-        Client existingClient = existingClientOptional.get();
-
-        // Créer une réservation avec l'offre et le client
-        Reservation reservation = new Reservation(
-                existingClient.getNom(),
-                existingClient.getPrenom(),
-                existingClient,
-                offre.getPrix()
-        );
-
-        // Sauvegarder la réservation
-        reservationRepository.save(reservation);
-
-        // Retourner une réponse appropriée
-        return ResponseEntity.ok("Réservation effectuée avec succès");
+        return reservationService.makeReservation(nomAgence, mdpAgence, offreId, nomClient, prenomClient);
     }
 
 
--- TPRest/src/main/java/Rest/Service/ReservationService.java
+++ TPRest/src/main/java/Rest/Service/ReservationService.java
@@ -1,94 +1,66 @@
 package Rest.Service;
 
 import Rest.Data.DataInitialization;
-import Rest.Models.*;
-import Rest.Repository.*;
+import Rest.Models.Client;
+import Rest.Models.Offre;
+import Rest.Models.Reservation;
+import Rest.Repository.ClientRepository;
+import Rest.Repository.OffreRepository;
+import Rest.Repository.ReservationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class ReservationService {
 
-    private final ReservationRepository reservationRepository;
-    private final OffreRepository offreRepository;
-    private final ClientRepository clientRepository; // Ajoutez le repository pour accéder à la base de données des clients
     private final DataInitialization dataInitialization;
+    private final OffreRepository offreRepository;
+    private final ClientRepository clientRepository;
+    private final ReservationRepository reservationRepository;
 
     @Autowired
-    public ReservationService(ReservationRepository reservationRepository, OffreRepository offreRepository, ClientRepository clientRepository, DataInitialization dataInitialization) {
-        this.reservationRepository = reservationRepository;
+    public ReservationService(DataInitialization dataInitialization, OffreRepository offreRepository, ClientRepository clientRepository, ReservationRepository reservationRepository) {
+        this.dataInitialization = dataInitialization;
         this.offreRepository = offreRepository;
         this.clientRepository = clientRepository;
-        this.dataInitialization = dataInitialization;
+        this.reservationRepository = reservationRepository;
     }
 
-    public ResponseEntity<String> makeReservation(String agenceNom, String motDePasse, int offreId, String nom, String prenom) {
-        // Vérifier l'authentification
-        boolean credentialsValid = dataInitialization.validateCredentials(agenceNom, motDePasse);
+    public ResponseEntity<String> makeReservation(String nomAgence, String mdpAgence, Long offreId, String nomClient, String prenomClient) {
+        boolean authentificationValide = dataInitialization.validateCredentials(nomAgence, mdpAgence);
 
-        if (!credentialsValid) {
-            // Gérer l'authentification invalide
-            return new ResponseEntity<>("Échec de l'authentification. Vérifiez les informations fournies.", HttpStatus.UNAUTHORIZED);
+        if (!authentificationValide) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
 
-        // Récupérer l'offre à partir de l'ID
-        Offre offre = offreRepository.findById((long) offreId).orElse(null);
-
-
-        if (offre == null || offre.getChambre() == null) {
-            // Gérer l'offre non trouvée ou la chambre non disponible
-            return new ResponseEntity<>("Échec de la réservation. L'offre n'est pas disponible.", HttpStatus.BAD_REQUEST);
+        Optional<Offre> offreOptional = offreRepository.findById(offreId);
+        if (offreOptional.isEmpty()) {
+            return ResponseEntity.status(404).body("L'offre spécifiée n'existe pas");
         }
 
-        if (!offre.getChambre().isDisponible()) {
-            // Gérer la chambre non disponible
-            return new ResponseEntity<>("Échec de la réservation. La chambre n'est pas disponible.", HttpStatus.BAD_REQUEST);
+        Offre offre = offreOptional.get();
+
+        Optional<Client> existingClientOptional = clientRepository.findByNomAndPrenom(nomClient, prenomClient);
+
+        if (existingClientOptional.isEmpty()) {
+            return ResponseEntity.status(400).body("Le client n'est pas enregistré. Impossible de faire la réservation.");
         }
 
+        Client existingClient = existingClientOptional.get();
 
-        // Rechercher le client dans la base de données en fonction du nom et du prénom
-        Client client = clientRepository.findByNomAndPrenom(nom, prenom).orElse(null);
+        Reservation reservation = new Reservation(
+                existingClient.getNom(),
+                existingClient.getPrenom(),
+                existingClient,
+                offre.getPrix()
+        );
 
-        if (client == null) {
-            // Gérer l'absence du client dans la base de données
-            return new ResponseEntity<>("Échec de la réservation. Client inconnu.", HttpStatus.BAD_REQUEST);
-        }
-
-        // Calculer le prix de la réservation (vous pouvez ajouter votre logique ici)
-        double prixReservation = calculateReservationPrice(offre);
-
-        // Créer la réservation
-        Reservation reservation = new Reservation(nom, prenom, client, prixReservation, offre.getDateDisponibilite(), offre.getDateExpiration(), offre);
-
-        // Enregistrer la réservation dans la base de données
         reservationRepository.save(reservation);
 
-        // Marquer l'offre comme réservée
-        offre.getChambre().setDisponible(false);
-        offreRepository.save(offre);
-
-        return new ResponseEntity<>("Réservation réussie. Référence de la réservation : " + reservation.getId(), HttpStatus.OK);
-    }
-
-    public ResponseEntity<String> checkAvailability(int offreId, String dateArrive, String dateDepart) {
-        // Recherche de l'offre dans la base de données
-        Offre offre = offreRepository.findById((long) offreId).orElse(null);
-
-        // Vérification de la disponibilité de l'offre
-       // if (offre == null || !offre.isDisponible()) {
-            // Gérer l'offre non trouvée ou non disponible
-         //   return new ResponseEntity<>("Échec de la réservation. L'offre n'est pas disponible.", HttpStatus.BAD_REQUEST);
-        //}
-
-        // L'offre est disponible
-        return new ResponseEntity<>("L'offre est disponible.", HttpStatus.OK);
-    }
-
-    private double calculateReservationPrice(Offre offre) {
-        // Ajoutez votre logique de calcul du prix de la réservation ici
-        // Par exemple, vous pouvez utiliser le prix de l'offre comme prix de la réservation
-        return offre.getPrix();
+        return ResponseEntity.ok("Réservation effectuée avec succès");
     }
 }
