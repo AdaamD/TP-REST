cb94112082a23ef1edfd08109822c7b124d15dc8
Adam DAIA <98329603+AdaamD@users.noreply.github.com>
2023-12-01 18:43:45 +0100

    Delete TPRest directory

--- TPRest/src/main/java/Rest/Repository/OffreRepository.java
+++
@@ -1,8 +0,0 @@
-package Rest.Repository;
-
-import Rest.Models.Offre;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OffreRepository extends JpaRepository<Offre,Long> {
-
-}
